bbce0fbcad368fd61417c3ce7187ae9c93335c0f
GIBDD-DPS <imperiapmk@gmail.com>
2026-04-17 15:41:34 +0300

    Create meta.py

---
+++ core/meta.py
@@ -0,0 +1,10 @@
+# ============================================
+# Prizolov Agent OS v2.001
+# Author: Dm.Andreyanov
+# Organization: Prizolov Market / Prizolov Lab
+# ============================================
+
+AUTHOR = "Dm.Andreyanov"
+ORG = "Prizolov Market / Prizolov Lab"
+VERSION = "2.001"
+SYSTEM_NAME = "Prizolov Agent OS"
